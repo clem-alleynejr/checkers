3f80cd191f5df798c12bfbfd411bd7dcee7c2886
clem-alleynejr <clemjr@hotmail.com>
2023-05-10 00:57:18 -0600

    about to change up inital setup so that html starts with having checkers on it

--- js/main.js
+++ js/main.js
@@ -96,12 +96,13 @@ let blackGraveyard;
 let redGraveyard;
 let mvDiagUpLeft; let mvDiagUpRight; // true if piece can move up and left/right diagonally
 let mvDiagDownLeft; let mvDiagDownRight; // true if piece can move down and left/right diagonally 
+// let selectedPiece;
 
 /*----- cached elements  -----*/
 const messageEl = document.querySelector('main h1')
 const playAgainBtnEl = document.getElementById('play-again')
 const endTurnBtnEl = document.getElementById('end-turn') 
-
+const squaresEl = co
 
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleClick);
@@ -112,31 +113,40 @@ document.getElementById('end-turn').addEventListener('click', nextTurn);
 /*----- functions -----*/
 init();
 
+
+
+
+
+
+
+
 function handleClick(evt) {
-    // Guards:
-    if (evt.target.className !== "black-piece" && evt.target.className !== "red-piece") return; // Need to click on checker piece
-    if (turn === 1 && evt.target.className !== "black-piece") return; // Black player cant pick a red piece
-    if (turn === -1 && evt.target.className !== "red-piece") return; // Red player can't pick a black piece
+    let onSquareRow = parseInt(evt.target.parentElement.id.substr(1,1));
+    let onSquareCol = parseInt(evt.target.parentElement.id.substr(3,3));
+    //Guard
+    if (board[onSquareRow][onSquareCol] !== turn) return;
     // Otherwise...
-    onSquareRow = evt.target.parentElement.id.substr(1,1);
-    onSquareCol = evt.target.parentElement.id.substr(3,3);
-    onSquare = board[onSquareRow][onSquareCol];
-    // if black:
-    if (evt.target.className === "black-piece") {
-        mvDiagUpLeft = countAdjacent(rowIdx, colIdx, -1, -1) === 0 ? true : false; // black can move up diagonal left if true
-        mvDiagUpRight = countAdjacent(rowIdx, colIdx, -1, 1) === 0 ? true : false; // black can move up diagonal right if true
-    };
-    // if red:
-    if (evt.target.className === "red-piece") {
-        mvDiagDownLeft = countAdjacent(rowIdx, colIdx, 1, -1) === 0 ? true : false; // red can move down diagonal left if true
-        mvDiagDownRight = countAdjacent(rowIdx, colIdx, 1, 1) === 0 ? true : false; // red can move down diagonal right if true
-    };
-    getAvailMoves();
+    console.log(evt.t)
+    console.log(board[onSquareRow][onSquareCol])
+    getAvailDiagMoves(onSquareRow, onSquareCol);
+    
+
+    
+    
 };
 
-function getAvailDiagMoves(evt) {
-
+function getAvailDiagMoves(onSquareRow, onSquareCol) {
+    const onSquare = board[onSquareRow][onSquareCol];
+    mvDiagUpLeft = (onSquareCol - 1 >= 0 && board[onSquareRow - 1][onSquareCol - 1] === 0) ? true : false;
+    mvDiagUpRight = (onSquareCol + 1 <= board[0].length - 1 && board[onSquareRow - 1][onSquareCol + 1] === 0) ? true : false;
+    console.log(mvDiagUpLeft, mvDiagUpRight) 
 };
+
+
+
+function getAvailMoves(evt) {
+    
+}
 
 
 function getAvailJumpMoves(evt) { 
@@ -182,6 +192,7 @@ function init() {
         [0, 0],
         [0, 0]
     ]
+    // selectedPiece = board[7][0]
     render();
 };
 
@@ -214,3 +225,128 @@ function renderControls() {
     playAgainBtnEl.style.visibility = winner ? 'visible' : 'hidden';
     endTurnBtnEl.style.visibility = afterMove ? 'visible' : 'hidden';
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function handleClick(evt) {
+//     // Guards:
+//     if (evt.target.className !== "black-piece" && evt.target.className !== "red-piece") return; // Need to click on checker piece
+//     if (turn === 1 && evt.target.className !== "black-piece") return; // Black player cant pick a red piece
+//     if (turn === -1 && evt.target.className !== "red-piece") return; // Red player can't pick a black piece
+//     // Otherwise...
+//     getAvailDiagMoves(evt);
+//     console.log(evt.target.parentElement.id.substr(1,1));
+//     console.log(onSquareRow);
+// };
+
+// function getAvailDiagMoves(evt) {
+//     const onSquareRow = evt.target.parentElement.id.substr(1,1);
+//     const onSquareCol = evt.target.parentElement.id.substr(3,3);
+//     const onSquare = board[onSquareRow][onSquareCol];
+//     // if black:
+//     if (evt.target.className === "black-piece") {
+//         mvDiagUpLeft = board[onSquareRow - 1][onSquareCol - 1] === 0 ? true : false; // black can move up diagonal left if true
+//         mvDiagUpRight = board[onSquareRow - 1][onSquareCol + 1] === 0 ? true : false; // black can move up diagonal right if true
+//     };
+//     // if red:
+//     if (evt.target.className === "red-piece") {
+//         mvDiagDownLeft = countAdjacent(onSquareRow, onSquareCol, 1, -1) === 0 ? true : false; // red can move down diagonal left if true
+//         mvDiagDownRight = countAdjacent(onSquareRow, onSquareCol, 1, 1) === 0 ? true : false; // red can move down diagonal right if true
+//     };
+// };
+
+
+
+
+
+
+
+
+// function handleClick(evt) {
+//     // let blackPiecesEl = [...document.querySelectorAll('.black-piece')]
+//     // let redPiecesEl = [...document.querySelectorAll('.red-piece')]
+//     let row = parseInt(evt.target.parentElement.id.substr(1,1));
+//     let col = parseInt(evt.target.parentElement.id.substr(3,3));
+//     if (board[row][col] !== turn) return;
+//     if (selectedPiece) {
+//         getAvailDiagMoves(row, col);
+//     }
+    
+
+//     // Otherwise...
+    
+//     console.log(mvDiagDownLeft);
+// };
+
+// function getAvailDiagMoves(row, col) {
+// console.log(row - 1, col + 1)
+//     const onSquare = board[row][col];
+//     // if black:
+//     if (turn === 1) {
+//         if (row - 1 >= 0) {
+//             mvDiagUpLeft = (col - 1 >= 0 && board[row - 1][col - 1] === 0) ? true : false;
+//             mvDiagUpRight = (col + 1 <= board[0].length - 1 && board[row - 1][col + 1] === 0) ? true : false;
+//         }
+//     };
+//     console.log(mvDiagUpLeft, mvDiagUpRight) ;
+//     // if red:
+//     // if (turn === -1 && redPieceNumber !== -1) {
+//     //     mvDiagDownLeft = countAdjacent(onSquareRow, onSquareCol, 1, -1) === 0 ? true : false; // red can move down diagonal left if true
+//     //     mvDiagDownRight = countAdjacent(onSquareRow, onSquareCol, 1, 1) === 0 ? true : false; // red can move down diagonal right if true
+//     // };
+// };
